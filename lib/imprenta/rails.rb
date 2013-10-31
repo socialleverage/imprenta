05f38dbf748ca2c9092006de3bef374b33e40841
Rafael Chacon <rafaelchacon@gmail.com>
2013-10-31 11:30:02 -0700

    Improve rails initialization process.

--- lib/imprenta/rails.rb
+++ lib/imprenta/rails.rb
@@ -1,1 +1,5 @@
-ActionController::Base.send :include, Imprenta::CachePage
+class Engine < Rails::Engine
+  initializer 'imprenta.controller_methods' do |app|
+    ActionController::Base.send :include, Imprenta::CachePage
+  end
+end
